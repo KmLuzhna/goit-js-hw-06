9f9c40fff74fada91429ba45a21aae2f21518d5b
Kristina Luzhna <luzhna09@gmail.com>
2022-07-12 22:15:51 +0300

    Done task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.forEach( element => {
+  const listOfIngrediets = document.querySelector('#ingredients');
+  const item = document.createElement("li");
+  item.textContent = element;
+  item.classList = "item";
+  console.log(item);
+  listOfIngrediets.append(item);
+});
