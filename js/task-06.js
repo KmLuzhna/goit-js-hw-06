21e50a610ba68f54bac7be30206f484a90c2ea9d
Kristina Luzhna <luzhna09@gmail.com>
2022-07-16 17:33:56 +0300

    Done task-05.js and task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const textInput = document.querySelector("input");
+console.log(textInput);
+
+const handleClick = (event) => {
+    const newTextInput = event.currentTarget.value;
+    const validLength = Number(textInput.getAttribute("data-length"))
+    if (newTextInput.length === validLength) {
+        textInput.classList.remove("invalid");
+        textInput.classList.add("valid");
+    }
+    else {
+        textInput.classList.remove("valid");
+        textInput.classList.add("invalid");  
+    }
+}
+textInput.addEventListener("blur", handleClick);
