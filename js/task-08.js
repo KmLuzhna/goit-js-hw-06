ce67815259ade8a90a211d7d767971d8877e32b1
Kristina Luzhna <luzhna09@gmail.com>
2022-07-17 14:25:59 +0300

    Done task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const loginForm = document.querySelector(".login-form");
 
+const submitForm = (event) => {
+    event.preventDefault();
+    const {elements: {email, password}} = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
+loginForm.addEventListener("submit", submitForm);
